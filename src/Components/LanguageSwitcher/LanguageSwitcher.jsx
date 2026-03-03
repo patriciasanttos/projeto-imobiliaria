358d622b2e9435b0268076046220162819c36079
Patricia Santos <patriciasanttos.dev@gmail.com>
2026-03-03 15:48:36 -0300

    icon flag changed

--- src/Components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/Components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -2,49 +2,14 @@ import { useState, useRef, useEffect } from "react";
 import { useLanguage } from "../../context/LanguageContext";
 import "./LanguageSwitcher.scss";
 
-// Flag SVG components
+import spainFlag from "../../assets/Icons/Flags/spain-flag-icon.png";
+import usaFlag from "../../assets/Icons/Flags/usa-flag-icon.png";
+import brazilFlag from "../../assets/Icons/Flags/brazil-flag-icon.png";
+
 const flags = {
-  es: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 14"
-      className="flag-icon"
-    >
-      <rect width="20" height="14" fill="#AA151B" />
-      <rect y="3.5" width="20" height="7" fill="#F1BF00" />
-    </svg>
-  ),
-  en: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 14"
-      className="flag-icon"
-    >
-      <rect width="20" height="14" fill="#B22234" />
-      <rect y="2" width="20" height="2" fill="#fff" />
-      <rect y="6" width="20" height="2" fill="#fff" />
-      <rect y="10" width="20" height="2" fill="#fff" />
-      <rect width="8" height="7.6" fill="#3C3B6E" />
-    </svg>
-  ),
-  pt: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 14"
-      className="flag-icon"
-    >
-      <rect width="20" height="14" fill="#009C3B" />
-      <polygon points="10,1.2 18.5,7 10,12.8 1.5,7" fill="#FEDF00" />
-      <circle cx="10" cy="7" r="2.8" fill="#002776" />
-      <path
-        d="M7.4,5.8 Q10,4.6 12.6,5.8"
-        stroke="#fff"
-        strokeWidth="0.7"
-        fill="none"
-        strokeLinecap="round"
-      />
-    </svg>
-  ),
+  es: <img src={spainFlag} alt="Español" className="flag-icon" />,
+  en: <img src={usaFlag} alt="English" className="flag-icon" />,
+  pt: <img src={brazilFlag} alt="Português" className="flag-icon" />,
 };
 
 const languages = [
